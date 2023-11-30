13def6e71282aeba8dda4323d8b16d9d743cac84
rosso101 <victorprivv101@gmail.com>
2023-11-30 11:32:43 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,28 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-class Dog:
-    pass
+    def __init__(self, name, breed):
+        self._name = name
+        self._breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be a string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in Dog.approved_breeds:
+            print("Breed must be in the list of approved breeds.")
+        else:
+            self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,28 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
-class Person:
-    pass
+   def __init__(self, name, job):
+        self._name = name
+        self._job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be a string under 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in Person.approved_jobs:
+            print("Job must be in the list of approved jobs.")
+        else:
+            self._job = value
